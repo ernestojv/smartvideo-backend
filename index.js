7b515052461d1b767f93de448b82df2ef9e57f20
Ernesto <ernestovanegas9@gmail.com>
2020-06-28 00:09:06 -0600

    Culminacion de la configuracion para la conexion con la base de datos en MongoAtlas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 const express = require('express');
 const app = express();
+//body parser
+app.use(express.json());
 
 moviesApi(app);
 
